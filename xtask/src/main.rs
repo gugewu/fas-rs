a57a670ab91df8fb48fe72dc7337ed360c63e492
gugewu <gugewu667@gmail.com>
2026-09-12 14:30:15 +0800

    Update main.rs

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -274,7 +274,7 @@ fn bin_path(release: bool) -> PathBuf {
 fn cargo_ndk() -> Command {
     let mut command = Command::new("cargo");
     command
-        .args(["+nightly", "ndk", "--platform", "31", "-t", "arm64-v8a", "--bindgen"])
+.args(["+nightly", "ndk", "--platform", "31", "-t", "arm64-v8a"])
         .env("RUSTFLAGS", "-C default-linker-libraries");
     command
 }
