296b19e368d18294531bf1d2bbdf9ecafed600a3
gugewu <gugewu667@gmail.com>
2026-09-12 13:13:41 +0800

    Update mod.rs

--- src/framework/config/data/mod.rs
+++ src/framework/config/data/mod.rs
@@ -1,20 +1,3 @@
-// Copyright 2024-2025, shadow3aaa
-//
-// This file is part of fas-rs.
-//
-// fas-rs is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// fas-rs is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// details.
-//
-// You should have received a copy of the GNU General Public License along
-// with fas-rs. If not, see <https://www.gnu.org/licenses/>.
-
 mod default;
 
 use std::collections::{HashMap, HashSet};
@@ -36,7 +19,7 @@ pub struct ConfigData {
     pub fast: ModeConfig,
 
     #[serde(default)]
-    pub controller_params: Option<ControllerParams>,   // 新增
+    pub controller_params: Option<ControllerParams>,
 }
 
 #[allow(clippy::struct_excessive_bools)]
